bafdae37043486328f9592350d9ca0e9e1625f84
wawa444 <baoran.eva@gmail.com>
2015-11-16 15:31:34 +0100

    add attachement

--- src/main/java/org/alma/csa/hadl/liens/AttachementFourni.java
+++ src/main/java/org/alma/csa/hadl/liens/AttachementFourni.java
@@ -1,7 +1,24 @@
 package org.alma.csa.hadl.liens;
 
+import org.alma.csa.hadl.composant.interfaces.port.Port;
+import org.alma.csa.hadl.composant.interfaces.port.PortComposantFourni;
+import org.alma.csa.hadl.composant.interfaces.port.PortConfigurationFourni;
+import org.alma.csa.hadl.connecteur.RoleRequis;
+
 /**
  * Created by jeremy on 16/11/15.
  */
 public class AttachementFourni extends Attachement {
+    private RoleRequis roleRequis;
+    private Port portFourni;
+
+    public AttachementFourni(RoleRequis roleRequis, PortComposantFourni portFourni) {
+        this.roleRequis = roleRequis;
+        this.portFourni = portFourni;
+    }
+
+    public AttachementFourni(RoleRequis roleRequis, PortConfigurationFourni portFourni) {
+        this.roleRequis = roleRequis;
+        this.portFourni = portFourni;
+    }
 }
--- src/main/java/org/alma/csa/hadl/liens/AttachementRequis.java
+++ src/main/java/org/alma/csa/hadl/liens/AttachementRequis.java
@@ -1,7 +1,24 @@
 package org.alma.csa.hadl.liens;
 
+import org.alma.csa.hadl.composant.interfaces.port.Port;
+import org.alma.csa.hadl.composant.interfaces.port.PortComposantRequis;
+import org.alma.csa.hadl.composant.interfaces.port.PortConfigurationRequis;
+import org.alma.csa.hadl.connecteur.RoleFourni;
+
 /**
  * Created by jeremy on 16/11/15.
  */
 public class AttachementRequis extends Attachement {
+    private RoleFourni roleFourni;
+    private Port portRequis;
+
+    public AttachementRequis(RoleFourni roleFourni, PortComposantRequis portRequis) {
+        this.roleFourni = roleFourni;
+        this.portRequis = portRequis;
+    }
+
+    public AttachementRequis(RoleFourni roleFourni, PortConfigurationRequis portRequis) {
+        this.roleFourni = roleFourni;
+        this.portRequis = portRequis;
+    }
 }
